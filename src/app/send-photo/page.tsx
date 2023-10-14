03a64626f77bee5b39b4a36ce38e1a04ed5968ab
scottchen98 <scott.chen1117@gmail.com>
2023-10-14 16:29:09 -0700

    finish send photo page

--- src/app/send-photo/page.tsx
+++ src/app/send-photo/page.tsx
@@ -13,7 +13,7 @@ export default function SendPhoto() {
         <Link href="/" className="absolute left-0">
           <ChevronLeft />
         </Link>
-        <h1 className="text-3xl font-bold">Inbox</h1>
+        <h1 className="text-3xl font-bold">Amy Smith</h1>
       </div>
       <div className="flex items-center justify-between px-5">
         <Avatar className="mx-auto h-[163.7px] w-[163.7px] border-4 border-secondary">
@@ -27,12 +27,19 @@ export default function SendPhoto() {
       <div>
         <RegularButton
           variant="right"
-          className="flex h-auto w-full max-w-sm flex-col items-start justify-start gap-5 pb-3 pr-3 pt-10"
+          className="navbar-gradient flex h-auto max-h-72 w-full max-w-sm flex-col items-start justify-start gap-5 space-y-10 border-none pb-0 pr-3 pt-10"
         >
           <h2 className="mx-auto text-2xl font-normal">
             Send <span className="font-bold">Amy</span> a photo.
           </h2>
-          <div className="relative z-10 ml-auto mt-auto h-16 w-16 overflow-hidden"></div>
+          <div className="relative bottom-0 left-1/2 z-10 mt-auto h-16 w-16 -translate-x-1/2 overflow-hidden">
+            <Image
+              src="/images/assets/leftArrow3d.png"
+              fill={true}
+              alt="Inbox icon"
+            />
+          </div>
+          <div className="weird-circle3 absolute bottom-0 left-1/2 h-[80px] w-[110px] -translate-x-[47%] transform overflow-hidden bg-secondary"></div>
         </RegularButton>
       </div>
 
