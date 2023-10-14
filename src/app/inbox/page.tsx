61c07a3bbf6dc966e17ae0c6e86ab2e931da3764
scottchen98 <scott.chen1117@gmail.com>
2023-10-14 14:00:06 -0700

    add top nav

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,3 +1,11 @@
 export default function Inbox() {
-  return <h1>Inbox page</h1>;
+  return (
+    <main className="mx-auto max-w-sm space-y-10 px-2 py-5">
+      <div className="flex items-center justify-between px-5">
+        <span>arrow</span>
+        <h2 className="text-3xl font-bold">Inbox</h2>
+        <span>SVG</span>
+      </div>
+    </main>
+  );
 }
