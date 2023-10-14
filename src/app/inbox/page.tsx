ce639e7ff6d32a7655c4eed3068e4f9479d0d33c
Michael Lei <michaelleie0ca@gmail.com>
2023-10-14 15:17:21 -0700

    Finish inbox scrolling

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,10 +1,37 @@
+import Image from "next/image";
+
+import { ChevronLeft } from "lucide-react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
+
 export default function Inbox() {
   return (
-    <main className="mx-auto max-w-sm space-y-10 px-2 py-5">
-      <div className="flex items-center justify-between px-5">
-        <span>arrow</span>
-        <h2 className="text-3xl font-bold">Inbox</h2>
-        <span>SVG</span>
+    <main className="mx-auto max-h-screen max-w-lg overflow-hidden px-2">
+      <div className="relative flex items-center justify-center py-5">
+        <Link href="/" className="absolute left-0">
+          <ChevronLeft />
+        </Link>
+        <h1 className="text-3xl font-bold">Inbox</h1>
+      </div>
+
+      <div className="h-[90vh] snap-y snap-mandatory overflow-y-scroll">
+        <div className="relative h-[90vh] w-full snap-center overflow-hidden bg-muted">
+          <Image
+            src="/images/photos/photo-1.jpg"
+            alt="Palm trees on a beach"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="relative h-[90vh] w-full snap-center overflow-hidden bg-muted">
+          <Image
+            src="/images/photos/photo-2.png"
+            alt="Palm trees on a beach"
+            fill
+            className="object-cover"
+          />
+        </div>
       </div>
     </main>
   );
