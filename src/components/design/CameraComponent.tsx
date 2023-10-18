abc1e0da75f453048eb5c3878f112160f1faa9d9
AliceEHZz <meiyellmya@gmail.com>
2023-10-18 11:27:18 -0700

    showing preview only when user capture from webcam

--- src/components/design/CameraComponent.tsx
+++ src/components/design/CameraComponent.tsx
@@ -1,27 +1,21 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function CameraComponent() {
-  let video: HTMLVideoElement | null = null;
-  let canvas: HTMLCanvasElement | null = null;
-  let photo: HTMLImageElement | null = null;
-
+  const [image, setImage] = useState("");
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const photoRef = useRef<HTMLImageElement>(null);
 
   const getUserCamera = () => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then((stream) => {
-        let video = videoRef.current;
-
-        if (video) {
+        if (videoRef.current) {
           //attach the stream to the video tag
-          video.srcObject = stream;
-          video.play();
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
         }
       })
       .catch((err) => {
@@ -35,55 +29,37 @@ export default function CameraComponent() {
     let width = 494;
     let height = 370;
 
-    video = videoRef.current;
-    canvas = canvasRef.current;
-    photo = photoRef.current;
+    if (videoRef.current && canvasRef.current) {
+      console.log("Take photo");
+      // set the photo width and height
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
 
-    if (canvas && video && photo) {
-      //set the photo width and height
-      canvas.width = width;
-      canvas.height = height;
-
-      let context = canvas.getContext("2d"); //image doesn't have getContext(), use canvas
+      let context = canvasRef.current.getContext("2d"); //image doesn't have getContext(), use canvas
       if (context) {
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        // convert canvas to image in png
-        const data = canvas.toDataURL("image/png");
-        photo.setAttribute("src", data);
-        photo.setAttribute("width", String(width));
-        photo.setAttribute("height", String(height));
+        context.drawImage(
+          videoRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height,
+        );
+        // convert canvasRef.current to image in png
+        const data = canvasRef.current.toDataURL("image/png");
+        setImage(data);
+        console.log("photos");
       }
     }
   };
 
-  // clear photo
-  function clearphoto() {
-    if (canvas) {
-      const context = canvas.getContext("2d");
-      if (context) {
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        const data = canvas.toDataURL("image/png");
-        if (photo) {
-          photo.setAttribute("src", data);
-        }
-      }
-    }
-  }
-
   useEffect(() => {
     getUserCamera();
-  }, [videoRef]);
+  }, []);
 
   return (
     <>
       <div className="mb-4 h-full w-full">
-        <video
-          ref={videoRef}
-          id="video"
-          className="h-full w-full border border-black shadow"
-        >
+        <video ref={videoRef} id="video" className="h-full w-full shadow">
           Video stream not available.
         </video>
         <button
@@ -91,30 +67,26 @@ export default function CameraComponent() {
           className="bg-grey-100 border-grey-500 text-grey-200 mx-auto mt-2 block rounded-full border px-4 py-2 text-sm shadow"
           onClick={takePicture}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-3.5 w-3.5"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-          >
-            <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
-            <span>Take photo</span>
-          </svg>
+          Take photo
         </button>
       </div>
-      {/* //! {should be moved to photo-preview somehow} */}
-      <div className="mb-4 h-auto w-auto">
-        <canvas ref={canvasRef} id="canvas" className="hidden"></canvas>
-        <div className="h-full w-full">
-          <Image
-            id="photo"
-            ref={photoRef}
-            src=""
-            alt="The screen capture will appear in this box."
-            className="border border-black shadow"
-          />
-        </div>
-      </div>
+
+      {image && <h4>Preview</h4>}
+      <canvas ref={canvasRef} id="canvas" className=""></canvas>
+      {image && (
+        <>
+          {" "}
+          <button className="mr-10 rounded-xl bg-blue-500 p-3 text-white">
+            Upload photo
+          </button>
+          <button
+            className="rounded-xl bg-blue-500 p-3 text-white"
+            onClick={takePicture}
+          >
+            Retake
+          </button>
+        </>
+      )}
     </>
   );
 }
