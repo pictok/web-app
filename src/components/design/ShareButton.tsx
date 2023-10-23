d0cf6e0d61608f3aa968824aaea17e51c2b5906a
Michael Lei <michaelleie0ca@gmail.com>
2023-10-22 20:14:14 -0700

    Replace share popover with dialog component

--- src/components/design/ShareButton.tsx
+++ src/components/design/ShareButton.tsx
@@ -1,16 +1,38 @@
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function ShareButton({ shareUrl }: { shareUrl: string }) {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(shareUrl);
+  };
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="default">Share</Button>
-      </PopoverTrigger>
-      <PopoverContent>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Share</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Share photo</DialogTitle>
+          <DialogDescription>
+            Share this link with your friends to send them the photo!
+          </DialogDescription>
+        </DialogHeader>
         <Input value={shareUrl} />
-      </PopoverContent>
-    </Popover>
+        <DialogFooter>
+          <Button variant="secondary" onClick={copyToClipboard}>
+            Copy
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
