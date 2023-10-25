1435bd5ca003870b7a01f2945e5ac72d261f4489
Michael Lei <michaelleie0ca@gmail.com>
2023-10-25 00:01:42 -0700

    Add lambda testing page for cors error

---
+++ src/app/test/lambda/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function LambdaTest() {
+  const handleClick = async () => {
+    const res = await fetch(
+      "https://tjv5dlrj1a.execute-api.us-west-2.amazonaws.com/default/pictok-text-to-sound",
+      {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ caption: "A dog is barking" }),
+      },
+    );
+    const { output } = await res.json();
+    console.log(output);
+  };
+  return (
+    <div className="container">
+      <Button onClick={handleClick}>Test</Button>
+    </div>
+  );
+}
