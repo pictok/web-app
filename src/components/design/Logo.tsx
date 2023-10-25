4574ded47f54578316a23526ccab5c665035e2d6
Michael Lei <michaelleie0ca@gmail.com>
2023-10-24 22:51:39 -0700

    Remove unused imports

--- src/components/design/Logo.tsx
+++ src/components/design/Logo.tsx
@@ -1,21 +1,10 @@
-import Image from "next/image";
-import { Baloo_2 } from "next/font/google";
 import Link from "next/link";
 import LogoIcon from "../icons/LogoIcon";
-
-const ballo2 = Baloo_2({ subsets: ["latin"] });
 
 export default function Logo() {
   return (
     <Link href="/" className="flex">
       <LogoIcon />
-      {/* <Image
-        src="/images/assets/pictok.png"
-        className="mb-5"
-        alt="Pictok Logo"
-        width={40}
-        height={40}
-      /> */}
     </Link>
   );
 }
