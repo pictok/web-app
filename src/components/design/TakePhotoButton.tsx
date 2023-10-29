7e0f9ec5712e27956592aa3e3ccac94bf265e070
Michael Lei <michaelleie0ca@gmail.com>
2023-10-29 16:22:54 -0700

    Finish take-photo on click

--- src/components/design/TakePhotoButton.tsx
+++ src/components/design/TakePhotoButton.tsx
@@ -3,11 +3,34 @@
 import { useRef } from "react";
 import Image from "next/image";
 import PrimaryButton from "./PrimaryButton";
+import { supabase } from "@/db/supabase";
+import { randomImageName } from "@/lib/randomImageName";
+import { redirect } from "next/navigation";
 
 export default function TakePhotoButton() {
   const photoInputRef = useRef<HTMLInputElement>(null);
-  const handleClick = () => {
+  const handleClick = async () => {
+    //Open the camera
     photoInputRef.current?.click();
+    //Get the photo
+    const files = photoInputRef.current?.files;
+    if (!files || files.length == 0) return;
+    const photo = files[0];
+    //Generate a random image name
+    const imageName = randomImageName() + photo.type.replace("image/", ".");
+    //Upload the photo to supabase storage
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(imageName, photo);
+    if (error) {
+      console.error(error);
+      return;
+    }
+    //Get the photo url
+    const photoUrl = data?.path;
+    console.log(photoUrl);
+    //Redirect the user to the photo processing page.
+    redirect("/");
   };
   return (
     <>
