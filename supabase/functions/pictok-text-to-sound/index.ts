98345a1d8989736ebb921237e429546714e8be9e
scottchen98 <scott.chen1117@gmail.com>
2023-10-30 00:14:19 -0700

    finish edge function

--- supabase/functions/pictok-text-to-sound/index.ts
+++ supabase/functions/pictok-text-to-sound/index.ts
@@ -2,19 +2,34 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log("Hello from Functions!")
+import Replicate from "https://esm.sh/replicate@0.20.1";
+
+const replicate = new Replicate({
+  auth: Deno.env.get("REPLICATE_API_TOKEN"),
+});
+
+const model =
+  "sepal/audiogen:154b3e5141493cb1b8cec976d9aa90f2b691137e39ad906d2421b74c2a8c52b8";
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+  const { caption } = await req.json();
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  const input = {
+    prompt: caption,
+    duration: 3,
+    top_k: 250,
+    top_p: 0,
+    temperature: 1,
+    classifier_free_guidance: 3,
+    output_format: "mp3",
+  };
+
+  const output = await replicate.run(model, { input });
+
+  return new Response(JSON.stringify({ output }), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
