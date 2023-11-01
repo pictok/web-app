360958efe2d9e45248855267af010fdee480dd92
scottchen98 <scott.chen1117@gmail.com>
2023-10-31 22:37:14 -0700

    show processing indicator during convertion

--- src/app/photo-processing/page.tsx
+++ src/app/photo-processing/page.tsx
@@ -23,6 +23,55 @@ export default function PhotoProcessing({
   const [shareUrl, setShareUrl] = useState("");
   const [caption, setCaption] = useState("");
 
+  const handleConversionToSound = async () => {
+    setIsConverting(true);
+
+    // get caption from image url
+    const res1 = await getCaption(photoString);
+    const data = await res1.json();
+    const caption = formatCaption(String(data.output));
+    setCaption(caption);
+
+    //use speech to text web api to read caption to the user
+    "speechSynthesis" in window
+      ? readCaption(caption)
+      : console.error("SpeechSynthesis is not supported in this browser.");
+
+    // get sound from caption
+    const { output } = await getSound(caption);
+
+    setSound(output);
+
+    // upload sound to supabase storage
+    const res3 = await fetch(output);
+    const blob = await res3.blob();
+    const audioName = `${Math.random()}.mp3`.replace("/", "");
+    const { error: SoundUploadError } = await supabase.storage
+      .from("audio")
+      .upload(audioName, blob);
+    if (SoundUploadError) console.log(SoundUploadError);
+
+    // insert image and audio url to supabase
+    const audioPath =
+      "https://bmtbohuzvkdifffdwayv.supabase.co/storage/v1/object/public/audio/";
+
+    const { data: result, error: CreateImgAudioLinkError } = await supabase
+      .from("image_audio")
+      .insert([
+        { image_url: photoString, audio_url: audioPath + audioName, caption },
+      ])
+      .select()
+      .single();
+    if (CreateImgAudioLinkError) console.log(CreateImgAudioLinkError);
+    if (result) {
+      setShareUrl(`${location.origin}/photo/${result.share_id}`);
+      setIsConverting(false);
+      const audio = new Audio(output);
+      await audio.play();
+    }
+  };
+  //   handleConversionToSound();
+
   return (
     <main className="mx-auto max-h-screen max-w-lg overflow-hidden px-2">
       <div className="relative flex items-center justify-center py-5">
@@ -38,15 +87,19 @@ export default function PhotoProcessing({
             src={photoPublicUrl}
             alt="Palm trees on a beach"
             fill
-            className="object-cover opacity-70"
+            className={`object-cover ${isConverting ? "opacity-70" : ""}`}
           />
         </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <LoadSpinnerSVG />
-        </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl">Processing</h1>
-        </div>
+        {isConverting && (
+          <>
+            <div className="absolute inset-0 flex items-center justify-center">
+              <LoadSpinnerSVG />
+            </div>
+            <div className="absolute inset-0 flex items-center justify-center">
+              <h1 className="text-4xl">Processing</h1>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
