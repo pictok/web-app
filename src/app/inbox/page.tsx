8dcd5480147accc071db2adb4709260285ea49a0
AliceEHZz <meiyellmya@gmail.com>
2023-11-06 21:22:44 -0800

    create branch

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -10,14 +10,12 @@ import { useEffect, useState } from "react";
 
 export default  function Inbox() {
   const [inbox, setInbox] = useState<any[]>([]);
-
-
   useEffect(() => {
     async function getInbox() {
       let { data, error } = await supabase.from("image_audio").select('*')
-  if (!data) {
-  console.log(error);
-  return;
+    if (!data) {
+      console.log(error);
+      return;
     }
     setInbox(data);
   }
@@ -38,6 +36,7 @@ export default  function Inbox() {
       <div className="h-[90vh] snap-y snap-mandatory overflow-y-scroll">
         {inbox.map((item) => (
           <div key={item.id} className="relative h-[90vh] w-full snap-center overflow-hidden bg-muted">
+            {/*  */}
             <img 
             src={item.image_url}
             alt="Palm trees on a beach"
