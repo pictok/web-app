e03c64414d3dccc9e177ef7ed82a44b1f061d910
AliceEHZz <meiyellmya@gmail.com>
2023-11-06 21:39:02 -0800

    Use ImageWithSound component in inbox

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,28 +1,27 @@
 "use client";
 import Image from "next/image";
 import { ChevronLeft } from "lucide-react";
-import {supabase} from "@/db/supabase";
-
-
+import { supabase } from "@/db/supabase";
+import ImageWithSound from "@/components/design/ImageWithSound";
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default  function Inbox() {
+export default function Inbox() {
   const [inbox, setInbox] = useState<any[]>([]);
+
   useEffect(() => {
     async function getInbox() {
-      let { data, error } = await supabase.from("image_audio").select('*')
-    if (!data) {
-      console.log(error);
-      return;
+      let { data, error } = await supabase.from("image_audio").select("*");
+      if (!data) {
+        console.log(error);
+        return;
+      }
+      setInbox(data);
     }
-    setInbox(data);
-  }
-  getInbox();
-    
-  }, []);
 
+    getInbox();
+  }, []);
 
   return (
     <main className="mx-auto max-h-screen max-w-lg overflow-hidden px-2">
@@ -35,13 +34,22 @@ export default  function Inbox() {
 
       <div className="h-[90vh] snap-y snap-mandatory overflow-y-scroll">
         {inbox.map((item) => (
-          <div key={item.id} className="relative h-[90vh] w-full snap-center overflow-hidden bg-muted">
-            {/*  */}
-            <img 
-            src={item.image_url}
-            alt="Palm trees on a beach"
-            className="object-cover"
+          <div
+            key={item.id}
+            className="relative h-[90vh] w-full snap-center overflow-hidden bg-muted"
+          >
+            <ImageWithSound
+              image_url={item.image_url}
+              audio_url={item.audio_url}
+              caption={item.caption}
+              className="object-cover"
             />
+            {/* <img
+              src={item.image_url}
+              onClick={() => {}}
+              alt={item.caption}
+              className="object-cover"
+            /> */}
           </div>
         ))}
       </div>
