d3146d6f38fa2b9b4fad20837349aac890901f13
Michael Lei <michaelleie0ca@gmail.com>
2023-11-07 23:51:16 -0800

    Add fake friends to the friends list
    Simulate a friends list by adding fake friends to the friends list.
    This will be replaced by a real friends list in a later commit.
    
     Co-authored-by: Scott Chen scott.chen1117@gmail.com

--- src/components/design/FriendList.tsx
+++ src/components/design/FriendList.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import FriendListItem from "./FriendListItem";
 
+const friends = ["Amy Smith", "Jane Doe", "John Doe"];
+
 export default function FriendList() {
   return (
     <div className="mt-5 space-y-10 px-5">
-      <Link href={"/send-photo"}>
-        <FriendListItem friend={"Amy Smith"} />
-      </Link>
+      {friends.map((friend) => (
+        <Link key={friend} href={"/send-photo"}>
+          <FriendListItem friend={friend} />
+        </Link>
+      ))}
     </div>
   );
 }
