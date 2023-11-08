cced0abcdb2b3514a6f9d53d1bbb943dba5ba2f3
Michael Lei <michaelleie0ca@gmail.com>
2023-11-07 23:23:42 -0800

    Add link to friendlist item

--- src/components/design/FriendList.tsx
+++ src/components/design/FriendList.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import FriendListItem from "./FriendListItem";
 
 const friendsList = ["Amy", "Lisa", "John", "Bob", "Sally"];
 export default function FriendList() {
   return (
-    <div className="flex flex-col items-center justify-center gap-5">
+    <div className="mt-5 flex flex-col items-center justify-center gap-5">
       {friendsList.map((friend) => (
-        <FriendListItem friend={friend} key={friend} />
+        <Link href={"/send-photo"} key={friend}>
+          <FriendListItem friend={friend} />
+        </Link>
       ))}
     </div>
   );
