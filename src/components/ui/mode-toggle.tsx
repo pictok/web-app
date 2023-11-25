471dec942aeeb9acb386f873a8db09af882c38ac
Michael Lei <michaelleie0ca@gmail.com>
2023-11-25 00:38:47 -0800

    Removed unused imports

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,19 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import SunIcon from "../icons/SunIcon";
 import MoonIcon from "../icons/MoonIcon";
-import { type } from "os";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
