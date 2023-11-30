7dbb08b924847c1af95fe970083a74c8eb0b459a
Jerry Fan <Jerryfan71@gmail.com>
2023-11-30 15:53:24 -0800

    finish layout

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,5 +1,19 @@
+import BackButton from "@/components/design/BackButton";
+import Navbar from "@/components/design/Navbar";
 import React from "react";
 
 export default function TutorialPage() {
-  return <h1>This is the Tutorial Page</h1>;
+  return (
+    <main className="mx-auto flex min-h-screen max-w-lg flex-col justify-between px-5">
+      <div>
+        <div className="relative flex items-center justify-center py-5">
+          <BackButton />
+          <h1 className="text-3xl font-bold">Tutorial</h1>
+        </div>
+      </div>
+      <div className="mb-5">
+        <Navbar />
+      </div>
+    </main>
+  );
 }
