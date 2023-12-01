84bcdb1c6456ed9f106287dde69f925faa9bfba0
Michael Lei <michaelleie0ca@gmail.com>
2023-11-30 17:35:27 -0800

    Make video responsive in regards to viewport width

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import BackButton from "@/components/design/BackButton";
-import Navbar from "@/components/design/Navbar";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export default function TutorialPage() {
   // State to mange if the video has played
@@ -18,26 +18,25 @@ export default function TutorialPage() {
   };
 
   return (
-    <main className="mx-auto flex min-h-screen max-w-lg flex-col justify-between px-5">
+    <main className="mx-auto flex max-h-screen max-w-lg flex-col justify-between px-5">
       <div>
         <div className="relative flex items-center justify-center py-5">
           <BackButton />
           <h1 className="text-3xl font-bold">Tutorial</h1>
         </div>
       </div>
-      <div>
-        <video
-          src="/video/Tutorial.mp4"
-          className="mb-4 max-h-[calc(100vh-8rem)] w-full rounded-lg object-cover"
-          autoPlay={!hasPlayed} // Autoplay only if it hasnt played yet
-          controls
-          onEnded={handleVideoEnd} // Call handleVideoEnd when the video ends
-        >
-          Your browser does not support the video
-        </video>
-      </div>
-      <div className="mb-5">
-        <Navbar />
+      <div className="mx-auto w-full max-w-lg">
+        <AspectRatio ratio={16 / 9}>
+          <video
+            src="/video/Tutorial.mp4"
+            className="w-full rounded-lg object-cover"
+            autoPlay={!hasPlayed} // Autoplay only if it hasnt played yet
+            controls
+            onEnded={handleVideoEnd} // Call handleVideoEnd when the video ends
+          >
+            Your browser does not support the video
+          </video>
+        </AspectRatio>
       </div>
     </main>
   );
