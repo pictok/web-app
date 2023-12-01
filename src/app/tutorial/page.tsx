966ebf607de8edf533746af4c323db5f22997ee7
Jerry Fan <Jerryfan71@gmail.com>
2023-11-30 16:25:31 -0800

    video autoplay and they can play or pause the video

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,8 +1,17 @@
+"use client";
 import BackButton from "@/components/design/BackButton";
 import Navbar from "@/components/design/Navbar";
-import React from "react";
+import { useState } from "react";
 
 export default function TutorialPage() {
+  // State to mange if the video has played
+  const [hasPlayed, setHasPlayed] = useState(false);
+
+  // handle for when the video has ends
+  const handleVideoEnd = () => {
+    setHasPlayed(true);
+  };
+
   return (
     <main className="mx-auto flex min-h-screen max-w-lg flex-col justify-between px-5">
       <div>
@@ -15,17 +24,15 @@ export default function TutorialPage() {
         <video
           src="/video/Tutorial.mp4"
           className="mb-4 max-h-[calc(100vh-8rem)] w-full rounded-lg object-cover"
-          autoPlay
+          autoPlay={!hasPlayed} // Autoplay only if it hasnt played yet
           controls
+          onEnded={handleVideoEnd} // Call handleVideoEnd when the video ends
         >
           Your browser does not support the video
         </video>
       </div>
       <div className="mb-5">
         <Navbar />
-      </div>
-      <div>
-        <h1>Test</h1>
       </div>
     </main>
   );
