7b8fb6947775cb3fd5f20f578417054d0c39be06
Michael Lei <michaelleie0ca@gmail.com>
2023-12-04 22:34:32 -0800

    Add notification sound when user receives a new message in their inbox

--- src/app/realtime-inbox.tsx
+++ src/app/realtime-inbox.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { useRealtime } from "@/providers/RealtimeProvider";
+import { useEffect, useRef } from "react";
 
 export default function RealtimeInbox() {
   const { numberOfUnreadImages } = useRealtime();
+
+  useEffect(() => {
+    const notification = new Audio("/sound/notification.mp3");
+    if (numberOfUnreadImages > 0) {
+      notification.play();
+    }
+    return () => {
+      notification.pause();
+    };
+  }, [numberOfUnreadImages]);
 
   return (
     <>
